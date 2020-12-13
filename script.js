55b1f2116ef2f35a62a1bea94f7e4cd126d928d5
Daniel Rubiano <danielrubianoam@gmail.com>
2020-12-12 19:31:48 -0500

    second

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
-
-
 function crear(){
-    var table ="<table align=\"center\" border=\"1\" id=\"datos1\">";
     var filas = document.getElementById("numprocesos").value
+    var table ="<table align=\"center\" border=\"1\" id=\"datos1\">";
 
     table+="<tr>";
     for(j=0;j<5;j++){ 
@@ -25,7 +23,7 @@ function crear(){
             if(i==0){
                 table+="<td id=\""+j+","+i+"\"  value=\""+j+","+i+"\">"+(j+1)+"</td>"
             }else if(i==3){
-                table+="<td><input type=\"number\" id=\""+j+","+i+"\" min=\"1\" max=\"19\"  value=\"1\"></td>"
+                table+="<td><input type=\"number\" id=\""+j+","+i+"\" min=\"0\" max=\"19\"  value=\"1\"></td>"
             }else{
                 table+="<td><input type=\"number\" id=\""+j+","+i+"\" min=\"0\" max=\"20\"  value=\"0\"></td>"
             }
@@ -39,10 +37,12 @@ function crear(){
     document.getElementById("datos").innerHTML=table;
 }
 
+var listaProcesos
+
 function simular(){
     var filas = document.getElementById("numprocesos").value
-    var listaProcesos = new Array(filas)
     var aux = new Array(5)
+    listaProcesos = new Array(filas)
 
     for(i=0;i<filas;i++){
         aux = [0,0,0,0,0]
@@ -52,21 +52,85 @@ function simular(){
             }else{
                 aux[j] = document.getElementById(i+","+j).value
             }
-            
         }
-        listaProcesos[i] = procesos(aux)
+        listaProcesos[i] = new procesos(aux)
+    }
+
+    FCFS()
+    SJF()
+    SRTF()
+
+}
+
+function procesos(aux){
+    this.id = aux[0]
+    this.insLlegada = aux[1]
+    this.duracion = aux[2]
+    this.inBloqueo = aux[3]
+    this.durBloqueo = aux[4]
+    var estado
+}
+   
+function FCFS(){
+    /*First come, first served*/
+    var filas = document.getElementById("numprocesos").value
+    var prioridad = new Array(filas)
+
+    for(i=0; i<filas; i++){
+        prioridad[i] = listaProcesos[i]
+    }
+
+    for (i=1; i<filas; i++) {
+        for (j=0; j<(filas-i); j++) {
+            if (prioridad[j].insLlegada > prioridad[j+1].insLlegada){
+                aux = prioridad[j];
+                prioridad[j] = prioridad[j+1];
+                prioridad[j+1] = aux;
+            }
+        }
     }
 }
 
+function SJF(){
+    /*Shorted Job First*/
+    var filas = document.getElementById("numprocesos").value
+    var prioridad = new Array(filas)
 
-function procesos(aux){
-    var id = aux[0]
-    var insLlegada = aux[1]
-    var duracion = aux[2]
-    var inBloqueo = aux[3]
-    var durBloqueo = aux[4]
+    for(i=0; i<filas; i++){
+        prioridad[i] = listaProcesos[i]
+    }
+
+    for (i=1; i<filas; i++) {
+        for (j=0; j<(filas-i); j++) {
+            if (prioridad[j].duracion > prioridad[j+1].duracion){
+                aux = prioridad[j];
+                prioridad[j] = prioridad[j+1];
+                prioridad[j+1] = aux;
+            }
+        }
+    }
 }
 
-function primerAlgoritmo(){
-    
+function SRTF(){
+    /*Short Remaining Time First*/
+    var filas = document.getElementById("numprocesos").value
+    var prioridad = new Array(filas)
+
+    for(i=0; i<filas; i++){
+        prioridad[i] = listaProcesos[i]
+    }
+
+    for (i=1; i<filas; i++) {
+        for (j=0; j<(filas-i); j++) {
+            ola = prioridad[j].duracion - prioridad[j].inBloqueo
+            ola2 = prioridad[j+1].duracion - prioridad[j+1].inBloqueo
+            if (ola > ola2){
+                aux = prioridad[j];
+                prioridad[j] = prioridad[j+1];
+                prioridad[j+1] = aux;
+            }
+        }
+    }
+
+    console.log(prioridad)
 }
